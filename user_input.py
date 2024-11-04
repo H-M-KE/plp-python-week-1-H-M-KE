eb579ec8fcb0cf4beeca5bbd191f3f877b2a53eb
H-M-KE <harrym254st@gmail.com>
2024-11-04 13:14:29 +0300

    DONE

---
+++ user_input.py
@@ -0,0 +1,17 @@
+
+# user_input.py
+
+# Ask the user for their name
+name = "Elijah"
+
+# Ask the user for their age
+age = "77"
+
+# Ask the user for their location
+location = "Zanzibar"
+
+# Print a personalized message
+print(f"Hello I am {name}, {age} years old and live in {location}.")
+
+
+
